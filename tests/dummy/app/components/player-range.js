e3a1eae05010fa8d0c02fbb5fc6ea5c200da97f6
dougbanville <doug.banville@rte.ie>
2019-05-04 09:27:26 +0100

    working basic range slider

--- tests/dummy/app/components/player-range.js
+++ tests/dummy/app/components/player-range.js
@@ -1,16 +1,34 @@
 import Component from "@ember/component";
 import layout from "../templates/components/player-range";
+import { inject as service } from "@ember/service";
+import { debounce } from "@ember/runloop";
 
 export default Component.extend({
   layout,
 
-  input(event) {
-    console.log(event.target.value);
-  },
+  emberFlowplayer: service(),
+
+  sliderValueB: 3,
 
   actions: {
-    sliderMoved(event) {
-      console.log(event.target.value); // the slider's value
+    sliderEvent(event) {
+      if (!this.emberFlowplayer.sliding) {
+        this.emberFlowplayer.setSliderState(true);
+      }
+      this.set("value", event.target.value);
+      //debounce(this.myFlowplayer.setSliderState(true));
+      //debounce(this.set("value", event.target.value));
+    },
+    select(event) {
+      //this.set("value", event.target.value);
+      this.emberFlowplayer.player.seek(event.target.value);
+      this.emberFlowplayer.setSliderState(false);
+    },
+    focusOut() {
+      console.log("DON");
+    },
+    sliderMovedExplicit(val) {
+      this.set("sliderValueB", val);
     }
   }
 });
