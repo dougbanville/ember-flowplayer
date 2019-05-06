36639046c3df5574b0472a36ca33374f14a1f52b
dougbanville <doug.banville@rte.ie>
2019-05-06 14:33:33 +0100

    volume range - throws error but works.

---
+++ addon/components/ember-flowplayer-volumerange.js
@@ -0,0 +1,44 @@
+import Component from "@ember/component";
+import layout from "../templates/components/ember-flowplayer-volumerange";
+import { inject as service } from "@ember/service";
+import noUiSlider from "nouislider";
+
+export default Component.extend({
+  layout,
+
+  tagName: "div",
+
+  elementId: "ember-flowplayer-volumerange",
+
+  emberFlowplayer: service(),
+
+  didInsertElement() {
+    var rangeSlider = document.getElementById("ember-flowplayer-volumerange");
+
+    noUiSlider
+      .create(rangeSlider, {
+        start: [this.value],
+        value: [this.value],
+        connect: [true, false],
+        behaviour: "tap-drag",
+        keyboardSupport: false,
+        range: {
+          min: [0],
+          max: [1]
+        }
+      })
+      .on("slide", () => {
+        this.set("sliding", true);
+      });
+
+    rangeSlider.noUiSlider.on("end", values => {
+      //this.emberFlowplayer.player.set("volume", 0);
+      //this.emberFlowplayer.player.volume(0.5);
+      let volume = parseFloat(values);
+      console.log(this.emberFlowplayer.player.conf.volume);
+      //this.emberFlowplayer.player.conf.volume = volume;
+      this.emberFlowplayer.player.volume(volume);
+      this.set("sliding", false);
+    });
+  }
+});
