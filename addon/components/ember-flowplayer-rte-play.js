e957cdc9ac404241219eea4ff1d82131fbfdf4e3
dougbanville <doug.banville@rte.ie>
2019-05-10 12:03:59 +0100

    use audioId not id

--- addon/components/ember-flowplayer-rte-play.js
+++ addon/components/ember-flowplayer-rte-play.js
@@ -12,9 +12,36 @@ export default Component.extend({
 
   actions: {
     play(model) {
-      //console.log(model.showid);
-      let url = `https://feeds.rasset.ie/rteavgen/getplaylist/?format=jsonp&id=${
-        model.showid
+      if (this.audioId < 100) {
+        this.send("playLive", model);
+      } else {
+        let url = `https://feeds.rasset.ie/rteavgen/getplaylist/?format=jsonp&id=${this.audioId}`;
+        fetchJsonp(url, {
+          jsonpCallbackFunction: "html5player"
+        })
+          .then(response => {
+            return response.json();
+          })
+          .then(json => {
+            //console.dir(json.shows[0]["media:group"][0]);
+            let hlsUrl = json.shows[0]["media:group"][0].hls_server + json.shows[0]["media:group"][0].hls_url;
+            this.set("hlsUrl", hlsUrl);
+            this.emberFlowplayer.change(true);
+            let audio = [{ type: this.type, src: this.hlsUrl }];
+            this.emberFlowplayer.setLive(this.audioId);
+            this.emberFlowplayer.player.load({
+              sources: audio
+            });
+            this.emberFlowplayer.setNowPlaying(model);
+          })
+          .catch(function(ex) {
+            alert("Could not find that audio");
+          });
+      }
+    },
+    playLive(model) {
+      let url = `https://feeds.rasset.ie/livelistings/playlist/?source=rte.ie&platform=webradio&channelid=${
+        this.audioId
       }`;
       fetchJsonp(url, {
         jsonpCallbackFunction: "html5player"
@@ -23,21 +50,19 @@ export default Component.extend({
           return response.json();
         })
         .then(json => {
-          //console.dir(json.shows[0]["media:group"][0]);
-          let hlsUrl =
-            json.shows[0]["media:group"][0].hls_server +
-            json.shows[0]["media:group"][0].hls_url;
+          console.log(json[0].fullUrl);
+          let hlsUrl = json[0].fullUrl;
           this.set("hlsUrl", hlsUrl);
           this.emberFlowplayer.change(true);
           let audio = [{ type: this.type, src: this.hlsUrl }];
-          this.emberFlowplayer.setLive(this.id);
+          this.emberFlowplayer.setLive(this.audioId);
           this.emberFlowplayer.player.load({
             sources: audio
           });
           this.emberFlowplayer.setNowPlaying(model);
         })
         .catch(function(ex) {
-          alert("Could not find that audio");
+          alert("couldn't find that live stream");
         });
     }
   }
